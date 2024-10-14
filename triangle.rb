6280a8b2434b202a7ebceac546c84611b410a486
Yunhao-Luo <yhluo101@gmail.com>
2024-10-13 21:12:21 -0700

    update test cases

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,9 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
 end
 
 # Error class used in part 2.  No need to change this code.
